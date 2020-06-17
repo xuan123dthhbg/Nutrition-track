4359dfbf629c8a880d3b804d43f369ec0bade363
Xuân Nguyễn <xuan123dthhbg@gmail.com>
2020-06-18 05:22:59 +0700

    user-profile

--- pages/profile.php
+++ pages/profile.php
@@ -8,32 +8,46 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 <body>
-<div class="header grid-container">
-    <a href="../index.php">
-        <div class="header-img"></div></a>
-    <button class="about btn">Giới thiệu</button>
-    <a href="../index.php">
-        <button class="login btn">Đăng xuất</button>
-    </a>
-</div>
-<div class="home">
-    <div class="home-header"></div>
+    <div class="header grid-container">
+        <a href="../index.php">
+            <div class="header-img"></div>
+        </a>
+        <button class="about btn">Giới thiệu</button>
+        <a href="../index.php">
+            <button class="logout btn">Đăng xuất</button>
+        </a>
+    </div>
+    <div class="home-body">
+        <div class="infor-bar">
+            <div class="avt">
+                <img src="../img/avatar.png" style="width: 200px; height: 200px">
+            </div>
+            <div class="infor-index">
+                <p style="font-weight: bolder; font-size: 30px; border-bottom: #111111 5px solid">Username</p>
+                <table style="width: 100%; font-size: 20px">
+                    <tr>
+                        <th>Chiều cao</th>
+                        <th>Cân nặng</th>
+                        <th>BMI</th>
+                        <th>TDEE</th>
+                    </tr>
+                     <tr align="center">
+                         <td>159cm</td>
+                         <td>56kg</td>
+                         <td>21.9</td>
+                         <td>1800KCal</td>
+                     </tr>
+                </table>
+            </div>
 
-    <table class="infor-bar">
-        <tr>
-            <th>159cm</th>
-            <th class="avt">
-                <img src="../img/avatar.png" style="width: 150px; height: 150px">
-                <p>Username</p>
-            </th>
-            <th>56kg</th>
-        </tr>
-    </table>
-        <div class="menu">
-            <button class="infor-btn">Hồ sơ của tôi</button> <br>
-            <button class="infor-btn">Nhật ký</button>
         </div>
 
-</div>
+        <div class="menu">
+            <a href="userInfor.html"><button class="menu-btn">Chỉnh sửa hồ sơ</button></a> <br>
+            <button class="menu-btn">Nhật ký của tôi</button> <br>
+            <button class="menu-btn">Mục tiêu</button> <br>
+            <button class="menu-btn">Đăng xuất</button> <br>
+        </div>
+    </div>
 </body>
 </html>
